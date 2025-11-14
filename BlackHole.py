1fb5e58faac7ce886c2d2a5e3cef871805c6e131
DirectedHunt42 <jackpwmurray@gmail.com>
2025-11-13 19:09:02 -0500

    Updated to version 1.1.2 to fix scaling issues

--- BlackHole.py
+++ BlackHole.py
@@ -24,7 +24,7 @@ APP_ICON_PATH = r"Icons\BlackHole_Icon.ico"
 BLACK_HOLE_LOGO = r"Icons\BlackHole_Transparent_Light.png"
 NOVA_FOUNDRY_LOGO = r"Icons\Nova_foundry_wide_transparent.png"
 LICENSE_TEXT = r"LICENSE.txt"
-VERSION = "1.1.1"
+VERSION = "1.1.2"
 
 # --- Paths ---
 local_appdata = os.getenv("LOCALAPPDATA") or os.getenv("APPDATA")
