16944319c164d512abd585d72da6907706daa7f8
DirectedHunt42 <jackpwmurray@gmail.com>
2025-11-20 20:35:35 -0500

    Attempted another fix and fixed windows icons

--- BlackHole.py
+++ BlackHole.py
@@ -241,7 +241,7 @@ class PasswordManager(ctk.CTk):
     def set_window_icon(window):
         if platform.system() == "Windows" and os.path.exists(APP_ICON_PATH):
             try:
-                window.iconbitmap(APP_ICON_PATH)
+                window.after(250, lambda: window.iconbitmap(APP_ICON_PATH))
             except:
                 pass
         elif platform.system() == "Linux" and os.path.exists(APP_ICON_PATH_LINUX):
